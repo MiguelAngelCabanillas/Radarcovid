b739f8d5fac5f4fb1ce11a4c267aad7137017c0d
Miguel Angel <0619851753@uma.es>
2020-12-04 21:17:58 +0100

    Tabla con datos

--- indexddd.php
+++ indexddd.php
@@ -1,17 +1,14 @@
 <?php
     
-    include 'db.php';
-    include 'display1.php';
     $conexion = mysqli_connect("localhost", "root", "root");
     mysqli_select_db($conexion,"radarcovid");
     $sql = "SELECT * FROM people";
     $resultado = mysqli_query($conexion, $sql) or die(mysqli_error());
 
-//    while($row = mysqli_fetch_array($resultado)) {
-  //      echo $row [ "id" ]." -> ".$row [ "lastname" ].", ".$row [ "firstname" ]."<br />";
+    //while($row = mysqli_fetch_array($resultado)) {
+      //  echo $row [ "id" ]." -> ".$row [ "lastname" ].", ".$row [ "firstname" ]."<br />";
     //}
     
-    mysqli_close($conexion);
 ?>
 
 
@@ -23,39 +20,53 @@
         <link rel="stylesheet" href="styles.css">
     </head>
     <body>
-        <!--GridView en html-->
-        <div class="gridview">
+        <div class="selectDisponibles">
             <form action="">
-                <label for="persona">Personas</label>
-                <!--Fila 1-->
-                <div class="row">
-                    <!--Columna 1-->
-                    <div class="column" style="background-color: #aaa;">
-                        <h2>Column1</h2>
-                        <p>Some text...</p>
-                    </div>
-                    <!--Columna 2-->
-                    <div class="column" style="background-color: #bbb;">
-                        <h2>Column2</h2>
-                        <p>Some text...</p>
-                    </div>
-                </div>
-                <!--Fila 2-->
-                <div class="row">
-                    <!--Columna 3-->
-                    <div class="column" style="background-color: #ccc;">
-                        <h2>Column3</h2>
-                        <p>Some text...</p>
-                    </div>
-                    <!--Columna 4-->
-                    <div class="column" style="background-color: #ddd;">
-                        <h2>Column4</h2>
-                        <p>Some text...</p>
-                    </div>
-                </div>
+                <label for="disponibles">Disponibles</label>
+                <select name="disponibles" id="disponibles" multiple>
+                    <!--Habria que generar el codigo dinamicamente, pongo algunos ejemplos-->
+                    <?php
+                    while($row = mysqli_fetch_array($resultado)) {
+                       echo "<option value=\"".$row [ "id" ]."\">".$row [ "lastname" ].", ".$row [ "firstname" ]."</option>";
+                    }
+                    ?>
+                    <!--<option value="mcrg">Tu Prima</option>-->
+                </select>
+                <br><br>
+                <input type="submit" value="Submit">
             </form>
         </div>
-
+    <table class="blueTable">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Apellidos</th>
+            <th>head4</th>
+        </tr>
+        </thead>
+        <tfoot>
+            <tr>
+                <td colspan="4">
+                    <div class="links"><a href="#">&laquo;</a> <a class="active" href="#">1</a> <a href="#">2</a> <a href="#">3</a> <a href="#">4</a> <a href="#">&raquo;</a></div>
+                </td>
+            </tr>
+        </tfoot>
+        <tbody>
+            <?php
+                $resultado = mysqli_query($conexion, $sql) or die(mysqli_error());
+                while($row = mysqli_fetch_array($resultado)) {
+                    printf('
+                        <tr>
+                            <td>%s</td>
+                            <td>%s</td>
+                            <td>%s</td>
+                            <td>%s</td>
+                        </tr>', $row["id"], $row["firstname"], $row["lastname"], "");
+                }
+            ?>
+        </tbody>
+    </table>
         <!--Seleccion Multiple de Amigos-->
         <div class="selectAmigos">
             <form action="">
@@ -73,23 +84,7 @@
         </div>
 
         <!--Seleccion Multiple de Disponibles-->
-        <div class="selectDisponibles">
-            <form action="">
-                <label for="disponibles">Disponibles</label>
-                <select name="disponibles" id="disponibles" multiple>
-                    <!--Habria que generar el codigo dinamicamente, pongo algunos ejemplos-->
-                    <?php
-                    while($row = mysqli_fetch_array($resultado)) {
-                       echo "<option value=\"".$row [ "id" ]."\">".$row [ "lastname" ].", ".$row [ "firstname" ]."</option>";
-                    }
-                    ?>
-
-                    <!--<option value="mcrg">Tu Prima</option>-->
-                </select>
-                <br><br>
-                <input type="submit" value="Submit">
-            </form>
-        </div>
+        
 
         <!--Botones-->
         <div class="botones">
---
+++ table.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>RastreoCovid19</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+
+
+<table class="blueTable">
+<thead>
+<tr>
+<th>head1</th>
+<th>head2</th>
+<th>head3</th>
+<th>head4</th>
+</tr>
+</thead>
+<tfoot>
+<tr>
+<td colspan="4">
+<div class="links"><a href="#">&laquo;</a> <a class="active" href="#">1</a> <a href="#">2</a> <a href="#">3</a> <a href="#">4</a> <a href="#">&raquo;</a></div>
+</td>
+</tr>
+</tfoot>
+<tbody>
+<tr>
+<td>cell1_1</td>
+<td>cell2_1</td>
+<td>cell3_1</td>
+<td>cell4_1</td>
+</tr>
+<tr>
+<td>cell1_2</td>
+<td>cell2_2</td>
+<td>cell3_2</td>
+<td>cell4_2</td>
+</tr>
+<tr>
+<td>cell1_3</td>
+<td>cell2_3</td>
+<td>cell3_3</td>
+<td>cell4_3</td>
+</tr>
+<tr>
+<td>cell1_4</td>
+<td>cell2_4</td>
+<td>cell3_4</td>
+<td>cell4_4</td>
+</tr>
+<tr>
+<td>cell1_5</td>
+<td>cell2_5</td>
+<td>cell3_5</td>
+<td>cell4_5</td>
+</tr>
+<tr>
+<td>cell1_6</td>
+<td>cell2_6</td>
+<td>cell3_6</td>
+<td>cell4_6</td>
+</tr>
+</tbody>
+</table>
+<body>
+</html>
